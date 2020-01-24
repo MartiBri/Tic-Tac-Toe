6af8b985fb17b56b4acde794eb803b3509a33e27
MartiBri <53783053+MartiBri@users.noreply.github.com>
2020-01-24 11:00:02 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+let createTable = function() {
+
+    //CREATE ALL HTML ELEMENTS FOR TABLE
+    let table = document.createElement("TABLE");
+    let row1 = document.createElement("TR");
+    let row2 = document.createElement("TR");
+    let row3 = document.createElement("TR");
+
+    let data1 = document.createElement("TD");
+    let data2 = document.createElement("TD");
+    let data3 = document.createElement("TD");
+
+    //CREATE THE TABLE WITH
+    row1.appendChild(data1);
+    row1.appendChild(data2);
+    row1.appendChild(data3);
+
+
+    row2.appendChild(data1);
+    row2.appendChild(data2);
+    row2.appendChild(data3);
+
+
+    row3.appendChild(data1);
+    row3.appendChild(data2);
+    row3.appendChild(data3);
+
+    table.appendChild(row1);
+    table.appendChild(row2);
+    table.appendChild(row3);
+
+    let content = document.getElementById("content");
+    content.appendChild(table);
+};
